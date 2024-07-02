86268a831749d9ba622603344e6a6a34b4d089e3
tenghaohuang <42682963+tenghaohuang@users.noreply.github.com>
2024-07-01 20:26:04 -0400

    Update affective curve plotting code

--- quant_utils.py
+++ quant_utils.py
@@ -3,6 +3,7 @@ import csv
 
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+import matplotlib.pyplot as plt
 stop_words = set(stopwords.words('english'))
 def remove_stop_words(text):
     word_tokens = word_tokenize(text)
@@ -98,3 +99,70 @@ def get_valence_score(infs):
         sum+= max(sub_scores)
         rt_l.append(max(sub_scores))
     return sum,rt_l
+
+
+import numpy as np
+
+
+def plot_fitting_curve(scores, polynomial, plotting=True, color=None):
+    x = positions = list(range(len(scores)))
+    # x = [score[0] for score in scores]
+    y = scores
+
+    # calculate polynomial
+    z = np.polyfit(x, y, polynomial)
+    f = np.poly1d(z)
+
+    # calculate new x's and y's
+    x_new = np.linspace(x[0], x[-1], 50)
+    y_new = f(x_new)
+
+    #     print(len(y_new))
+    if plotting:
+        plt.plot(x, y, 'o', x_new, y_new, color=color)
+        plt.xlim([x[0] - 1, x[-1] + 1])
+    return y_new
+
+
+def get_interpolated_overall_scores(inferences, polynomial=15):
+    if inferences == []:
+        return None, None
+    ensemble = []
+    for _ in range(len(inferences)):
+        try:
+            splited = [i.split(", ") for i in inferences[_]['st_emo']]
+        except:
+            print(_)
+            continue
+        aoursal_overall = []
+        valence_overall = []
+        for l in splited:
+            if len(l) != 3:
+                print(l)
+                continue
+            tmps = []
+            for i in l:
+                i = i.lower()
+                i = i.strip(".[] ")
+                i.replace('and ', '')
+                tmps.append(i)
+            # take the top 1 sentiment
+            tmps = tmps[:2]
+            a_score, a_rt_l, _ = get_arousal_score(tmps)
+            v_score, v_rt_l = get_valence_score(tmps)
+            if len(a_rt_l) != 0:
+                aoursal_overall.append(a_score / len(a_rt_l))
+            else:
+                last_item = aoursal_overall[-1]
+                aoursal_overall.append(last_item)
+            if len(v_rt_l) != 0:
+                valence_overall.append(v_score / len(v_rt_l))
+            else:
+                last_item = valence_overall[-1]
+                valence_overall.append(last_item)
+
+        ensemble.append([plot_fitting_curve(aoursal_overall, polynomial=polynomial, plotting=False), \
+                         plot_fitting_curve(valence_overall, polynomial=polynomial, plotting=False)])
+    average = np.mean(ensemble, axis=0)
+    return average, ensemble
+
